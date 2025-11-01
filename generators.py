d659935a98e682cdc88775e68a2bc96aa0db9877
mea <mahmuodimad@gmail.com>
2025-11-01 01:45:18 -0700

    added harmonic oscillator

--- generators.py
+++ generators.py
@@ -22,18 +22,20 @@ def single_well_generator(total_time,time_step, x0_mean, zeta, k, x_mu,T,
     '''
 
     torch.set_default_device(device)
-
-    x0 = torch.normal(mean=torch.tensor(x0_mean), std=torch.tensor(x0_std), size=(num_of_simulations,))  # sample the initial position
+    if x0_std == 0:
+        x0 = torch.full((num_of_simulations,), x0_mean, device=device)
+    else:
+        x0 = torch.normal(mean=torch.tensor(x0_mean), std=torch.tensor(x0_std), size=(num_of_simulations,))  # sample the initial position
 
     num_time_steps = int(total_time/time_step)  # calculates the number of time steps given the time
 
-    diffusion = torch.sqrt(torch.tensor(2*boltzmann*T/zeta))
+    diffusion = torch.sqrt(torch.tensor(2*boltzmann*T/zeta),device=device)
     dt_tensor = torch.tensor(time_step, device=device)
-    positions = torch.zeros((num_of_simulations,num_time_steps))  # use default float32
+    positions = torch.zeros((num_of_simulations,num_time_steps),device=device)  # use default float32
     positions[:, 0] = x0
     times = torch.arange(0, total_time, time_step, device=device)
 
-    dw = torch.normal(mean=0, std=np.sqrt(time_step), size=(num_of_simulations, num_time_steps-1))  # sample brownian motion increments
+    dw = torch.normal(mean=0, std=np.sqrt(time_step), size=(num_of_simulations, num_time_steps-1),device=device)  # sample brownian motion increments
 
     for i in range(1, num_time_steps):
         positions[:, i] = positions[:, i-1] + -(k/zeta) * (positions[:, i-1] - x_mu) * dt_tensor + diffusion * dw[:, i-1]
