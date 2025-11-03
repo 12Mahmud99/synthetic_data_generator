d82f4c202b5701efa8cfb9e6f4f73b9e4950d9be
mea <mahmuodimad@gmail.com>
2025-11-03 12:49:13 -0700

    added potential function visualization

---
+++ utils.py
@@ -0,0 +1,5 @@
+def graph_potential(positions,barrier_height,left_well,right_well,tilt):
+    midpoint = (left_well + right_well)/2.0 
+    a = abs(left_well-right_well)/2.0  
+    y=barrier_height*(((positions - midpoint)**4)/(a**4) - 2*(positions - midpoint)**2/(a**2)) +tilt*(positions-midpoint)
+    return y
